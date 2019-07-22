8e81930a076ad285270d4becbb657a4d88f5e976
Aman Kumar Singh <amanksingh01@gmail.com>
2019-07-22 15:11:27 +0530

    Updates

---
+++ precache-manifest.254caedfc58bc008468687ac8ecd8f16.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "88db1e79ae43e78f14d1a0866381b690",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "26b421c83feba6e65bce",
+    "url": "/monsters-rolodex/static/css/main.79b71bda.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/monsters-rolodex/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "26b421c83feba6e65bce",
+    "url": "/monsters-rolodex/static/js/main.cf42e2dc.chunk.js"
+  },
+  {
+    "revision": "c3f3f5c37da57c0082d3",
+    "url": "/monsters-rolodex/static/js/runtime~main.c2844b1e.js"
+  }
+]);
